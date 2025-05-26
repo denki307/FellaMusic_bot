6e10995d17c64d35463f40ac9520b12a606cd917
denki307 <mbtdenki@gmail.com>
2025-05-26 06:16:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/denki307/PRINCE-MUSIC",
+    "https://github.com/denki307/FellaMusic_bot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
